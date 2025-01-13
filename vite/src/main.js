8586a843a1a4980774c1971ebc1c043fb21c03ad
uzair16ahmed <khanuzair285@gmail.com>
2025-01-13 18:53:27 +0500

    Feat: handled window resizing

--- vite/src/main.js
+++ vite/src/main.js
@@ -13,11 +13,11 @@ camera.position.z = 5;
 
 // 3. Object
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({ color: '#468585', emissive: '#468585' });
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: '#B4B4B4' });
+const boxMaterial = new THREE.MeshStandardMaterial({ color: '#B4B4B4', emissive: '#B4B4B4' });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
@@ -54,5 +54,12 @@ function animate() {
 
     renderer.render(scene, camera);
 }
+
+// 8. Handle window resizing
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    camera.setSize(window.innerWidth, window.innerHeight);
+})
 
 animate()
